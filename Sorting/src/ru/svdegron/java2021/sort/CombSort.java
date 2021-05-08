0d05a24dcf3e1833e22625d6c307a4dcbd88d00a
svdegron <svdegron@gmail.com>
2021-05-08 18:30:05 +0500

    Added new class: 	- class name «CombSort»; 	- Rewritten override methods. Now method return zeros arrays, not null.

---
+++ Sorting/src/ru/svdegron/java2021/sort/CombSort.java
@@ -0,0 +1,23 @@
+package ru.svdegron.java2021.sort;
+
+public class CombSort extends Sorter {
+
+	@Override
+	public int[] AscendingFromBegin(int[] input) {
+
+		int[] output = new int[input.length];
+
+		return output;
+
+	}
+
+	@Override
+	public int[] DescendingFromEnd(int[] input) {
+
+		int[] output = new int[input.length];
+
+		return output;
+
+	}
+
+}
