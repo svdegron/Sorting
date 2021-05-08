5c5d82acb90da2d91b745d749e6f5416316afbcf
svdegron <svdegron@gmail.com>
2021-05-08 17:17:12 +0500

    Autoformating text. Added descending method into «CocktailSort»

--- Sorting/src/ru/svdegron/java2021/sort/CocktailSort.java
+++ Sorting/src/ru/svdegron/java2021/sort/CocktailSort.java
@@ -4,70 +4,97 @@ public class CocktailSort extends Sorter {
 
 	@Override
 	public int[] AscendingFromBegin(int[] input) {
-		
+
 		int[] output = input;
-		// remember array borders, for change check order 
+		// remember array borders, for change check order
 		int leftBorder = 0;
 		int rightBorder = output.length - 1;
-		
+
 		while (leftBorder < rightBorder) {
-			
+
 			int i = leftBorder;
-			
-			while(i < rightBorder) {
-				
+
+			while (i < rightBorder) {
+
 				i++; // at first line to not forget increment index at the end
-				
-				if(output[i - 1] > output[i]) {
-					
+
+				if (output[i - 1] > output[i]) {
+
 					int temp = output[i - 1];
 					output[i - 1] = output[i];
 					output[i] = temp;
-					
+
 				}
-				
+
 			}
-			
+
 			rightBorder--; // pushed max element into end, cut border from right
 			i = rightBorder;
-			
-			while(i > leftBorder) {
-				
+
+			while (i > leftBorder) {
+
 				i--;
-				
-				if(output[i + 1] < output[i]) {
-					
+
+				if (output[i + 1] < output[i]) {
+
 					int temp = output[i + 1];
 					output[i + 1] = output[i];
 					output[i] = temp;
-					
+
 				}
-				
+
 			}
-			
+
 			leftBorder++;
-						
+
 		}
-		
+
 		return output;
-		
+
 	}
 
 	@Override
 	public int[] DescendingFromEnd(int[] input) {
-		
-		int[] output = new int[input.length];
-		
-		/* example for change elements places
-		 * a = 2; b = 3;
-		 * a = a + b | 2 + 3 = 5
-		 * b = a - b | 5 - 3 = 2
-		 * a = a - b | 5 - 2 = 3
-		 * a = 3; b = 2
-		 */
+
+		int[] output = input;
+
+		int leftBorder = 0;
+		int rightBorder = output.length - 1;
+
+		while (leftBorder < rightBorder) {
+
+			for (int i = rightBorder; i > leftBorder; i--) {
+
+				//@formatter:off
+				if (output[i] > output[i - 1]) {				
+																// a = 2; b = 3
+					output[i]     = output[i] + output[i - 1]; 	// a = a + b | 2 + 3 = 5
+					output[i - 1] = output[i] - output[i - 1];	// b = a - b | 5 - 3 = 2
+					output[i]     = output[i] - output[i - 1];	// a = a - b | 5 - 2 = 3
+																// a = 3; b = 2
+				}
+				//@formatter:on
+			}
+
+			leftBorder++;
+
+			for (int i = leftBorder; i < rightBorder; i++) {
+
+				if (output[i] < output[i + 1]) {
+
+					output[i] = output[i] + output[i + 1];
+					output[i + 1] = output[i] - output[i + 1];
+					output[i] = output[i] - output[i + 1];
+				}
+
+			}
+
+			rightBorder--;
+
+		}
 
 		return output;
-		
+
 	}
 
 }
