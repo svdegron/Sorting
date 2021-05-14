bcf23c97aade80b4f07c96453b4857762c6f08d3
svdegron <svdegron@gmail.com>
2021-05-14 14:48:41 +0500

    My realization: 	- with helps on examples another language from Internet 	- does not matter, I think it's not right realization 	- I saw real realization

--- Sorting/src/ru/svdegron/java2021/sort/CombSort.java
+++ Sorting/src/ru/svdegron/java2021/sort/CombSort.java
@@ -2,44 +2,66 @@ package ru.svdegron.java2021.sort;
 
 public class CombSort extends Sorter {
 
+	// Optimal value basis on tests and practices (wiki)
+	private final double reductionFactor = 1.247330950103979;
+
 	@Override
 	public int[] AscendingFromBegin(int[] input) {
 
 		int[] output = input;
-		
+
 		int i = 0;
-		int j = output.length - 1;
-		
-		while(i < j) {
-			
-			System.out.print(i + " " + j + " | " + output[i] + " " + output[j]);
-			
-			if(output[i] > output[j]) {
+		int lastIndex = output.length - 1;
+		int offsetIndex = lastIndex;
+
+		int n = 0;
+
+		while (i < i + offsetIndex - 1) { // (… - 1) delete i equals j. If "<=" → infinity cycle
+
+			i = 0;
+			offsetIndex = (int) (offsetIndex / reductionFactor);
+
+			while (i + offsetIndex <= lastIndex) {
+
+				int j = offsetIndex + i;
 				
-				int temp = output[i];
-				output[i] = output[j];
-				output[j] = temp;
-				System.out.print(" → " + output[i] + " " + output[j] + "\n");
-				
-			} else {
-				System.out.print("\n");
+				System.out.print(i + " " + j + " | " + output[i] + " " + output[j]);
+
+				if (output[i] > output[j]) {
+					
+					int temp = output[i];
+					output[i] = output[j];
+					output[j] = temp;
+					
+					System.out.println(" → " + output[i] + " " + output[j]);
+					
+				} else {
+					System.out.print("\n");
+				}
+
+				i++;
+				n++;
+
 			}
-			
-			i++;
-			j++;
-			
-			if(j == output.length) {
-				
-				j = j - i - 1;
-				i = 0;
-				
-			}
-			
+
 		}
+
+		System.out.println(
+				"Кол-во проходов: " + n + " " + (105 - 60) + " " + (105 - 71) + " " + (105 - 85) + " " + (105 - 77));
 
 		return output;
 
 	}
+
+// example for Pascal
+//    Завожу цикл с условием «i < i + j», которое буквально означает «i отличается от i + j».
+//        Обнуляю i для того, чтобы при новом пробеге по массиву индекс не вышел за его границы.
+//        Завожу внутренний цикл с условием «i + j <= n», которое буквально значит
+//                  «сумма индекса i и расстояния j между a[i] и другим сравниваемым элементом не больше,
+//                  чем самый большой индекс массива».
+//            Если a[i] > a[i + j], то меняю их местами.
+//            Увеличиваю i.
+//        Уменьшаю j.
 
 	@Override
 	public int[] DescendingFromEnd(int[] input) {
