211b3ac2d6c570f86c13b130656b7a2dd4eda801
svdegron <svdegron@gmail.com>
2021-05-14 16:14:58 +0500

    Add branche: 	- add class 	- rewrite override methods (return not null)

---
+++ Sorting/src/ru/svdegron/java2021/sort/InsertionSort.java
@@ -0,0 +1,23 @@
+package ru.svdegron.java2021.sort;
+
+public class InsertionSort implements Sortable {
+
+	@Override
+	public int[] AscendingFromBegin(int[] input) {
+
+		int[] output = new int[input.length];
+
+		return output;
+
+	}
+
+	@Override
+	public int[] DescendingFromEnd(int[] input) {
+
+		int[] output = new int[input.length];
+
+		return output;
+
+	}
+
+}
